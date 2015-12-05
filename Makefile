1afef72b6f060d803045d3c3681cd50856ebf1bc
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2015-12-05 15:36:15 -0800

    History!

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CXX = clang++
 # CXXFLAGS = -std=c++11 -pthread
 CXXFLAGS = -std=c++11 -pthread
-LIBS = -lportaudio
+LIBS = -lportaudio -lreadline
 INC = -I/usr/local/include
 SRCS = main.cpp  mixer.cpp  oscillator.cpp  sawtooth.cpp  fm.cpp  triangle.cpp  utils.cpp bpmrrr.cpp algae.cpp
 OBJS = $(SRCS:.cpp=.o)
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <thread>
 
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <portaudio.h>
 
 #include "bpmrrr.h"
@@ -19,18 +21,15 @@ int main()
   thread {&Mixer::goMix, &mixer}.detach();
   thread {&BPMrrr::Run, &bpm}.detach();
 
-  string input_line;
-  while (1)
-  {
-    char *buf;
+  char *buf;
+  std::string prompt = std::string("\n") + COOL_COLOR_GREEN + "#> " + ANSI_COLOR_RESET;
 
-    while ((buf = readline("\n #> "))!= NULL) {
-    //cout << "\n" << COOL_COLOR_GREEN << "#> " << ANSI_COLOR_RESET;
-    //getline(cin, input_line);
-    //if ( cin.eof() )
-    //  exxy();
-        interpret(input_line);
-    }
-
+  while ((buf = readline(prompt.c_str()))!= NULL) {
+    if (buf[0] != 0)
+        add_history(buf);
+    interpret(buf);
   }
+
+  exxy();
+
 }
